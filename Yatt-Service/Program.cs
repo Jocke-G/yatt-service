6ac14bc9df80f880fac355bbf684a4adeb0c6ffd
Jocke-G <joakim.gumaelius@gmail.com>
2025-11-06 22:21:10 +0100

    Keycloak

--- Yatt-Service/Program.cs
+++ Yatt-Service/Program.cs
@@ -1,10 +1,18 @@
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Yatt_Service;
 using Yatt_Service.Repositories;
 using Yatt_Service.RepositoryInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keycloakOptions = builder.Configuration.GetSection("Keycloak").Get<KeycloakOptions>();
+
+builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+builder.Services.AddAuthorization().AddKeycloakAuthorization(builder.Configuration);
 
 builder.Services.AddDbContext<YattDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
@@ -24,24 +32,60 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri($"{keycloakOptions.AuthServerUrl}/realms/yatt/protocol/openid-connect/auth"),
+            }
+        }
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Keycloak",
+                    Type = ReferenceType.SecurityScheme,
+                },
+                In = ParameterLocation.Header,
+                Name = "Bearer",
+                Scheme = "Bearer",
+            },
+            Array.Empty<string>()
+        },
+    });
+});
 
 var app = builder.Build();
 
-if(builder.Configuration.GetValue<bool>("UseSwagger", false))
+app.UseAuthentication();
+app.UseAuthorization();
+
+if (builder.Configuration.GetValue("UseSwagger", false))
 {
     var option = new RewriteOptions();
     option.AddRedirect("^$", "swagger");
     app.UseRewriter(option);
-    app.UseSwaggerUI(x =>
+    app.UseSwaggerUI(options =>
     {
-        x.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Yet Anothes Template Thing");
+        options.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Yet Anothes Template Thing");
+        options.OAuthClientId(keycloakOptions.Resource);
     });
     app.UseSwagger();
 }
 
 app.UseCors();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
